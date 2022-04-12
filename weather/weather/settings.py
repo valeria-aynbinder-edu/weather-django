9a3aa233245fff422d9caea34edced5d530302a8
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-04-12 20:27:22 +0300

    sssss

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -84,15 +84,23 @@ WSGI_APPLICATION = 'weather.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'weather',
+#         'USER': 'deeptalk',
+#         'PASSWORD': 'deeptalk',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'weather',
-        'USER': 'deeptalk',
-        'PASSWORD': 'deeptalk',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase', # This is where you put the name of the db file.
+        # If one doesn't exist, it will be created at migration time.
     }
 }
 
